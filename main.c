882cfa2bd0c9ec48f369da7771a4e45d8c23250b
yjjieqc <lonelypiscesve@gmail.com>
2017-12-18 22:35:25 +0800

    uhash.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <limits.h>
+#include "uthash.h"
 //#include "otherfunction.h"
 //#include "headerfile1.h"
 //#include "headerfile2.h"
@@ -12,14 +13,53 @@
 #define debug 1
 
 
+struct numc {
+	int key;
+	int count;
+	UT_hash_handle hh;
+};
+
+struct numc *users = NULL;
+
+void add_user(int user_id, int count) {
+	struct numc *s;
+
+	s = malloc(sizeof(struct numc));
+	s->key = user_id;
+	s->count = 1;
+	HASH_ADD_INT(users, key, s);
+}
+struct my_struct *find_user(int user_id) {
+	struct my_struct *s;
+
+	HASH_FIND_INT(users, &user_id, s);  /* s: output pointer */
+	return s;
+}
+void delete_user(struct numc * user) {
+	HASH_DEL(users, user);
+	free(user);
+}
+void delete_all() {
+	struct my_struct *current_user, *tmp;
+
+	HASH_ITER(hh, users, current_user, tmp) {
+		HASH_DEL(users, current_user);  /* delete it (users advances to next) */
+		free(current_user);             /* free it */
+	}
+}
+
+int findShortestSubArray(int* nums, int numsSize) {
+
+}
+
 
 
 int main()
 {
-	int a[] = { -2,1,-3,4,-1,2,1,-5,4 };
+	int a[] = { 1,4,2,5,7,2,9 };
 #if debug
-	int b = maxSubArray(a, 9);
-	printf("%d\n", b);
+
+
 #endif
 	system("pause");
 	return 0;
